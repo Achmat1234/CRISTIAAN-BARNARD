a7fae2e02998e173dafad1284682df963abf4de1
Achmat <achmat.zaydan@gmail.com>
2019-07-01 09:32:53 +0200

    updated files

---
+++ main.js
@@ -0,0 +1,24 @@
+var i=0
+function read(){
+   if(!i){
+       Document. getElementById("more").style.display="inline";
+       Document. getElementById("dots").style.display="none";
+       Document. getElementById("read").innerHTML="Read Less";
+       i=1;
+       Document. getElementById("more").style.display="inline";
+       Document. getElementById("dots").style.display="none";
+       Document. getElementById("read").innerHTML="Read More";
+       i=0;
+   }
+
+
+
+
+
+
+
+
+
+
+
+}
